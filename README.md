0ab40520fd7533691e45eb0681297ea44d07ddaa
Jonathan Packer <jsp2151@columbia.edu>
2012-10-05 12:13:30 -0400

    Generated blank app

---
+++ README.md
@@ -0,0 +1,10 @@
+Geocraft: Visualizing Etsy Product Shipping
+===========================================
+
+An app made\* by:
+- Jonathan Packer
+- [enter name here]
+- [enter name here]
+- [enter name here]
+
+\* app not actually made yet
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocraft::Application.config.secret_token = '449207d8e07e4437a6be9cb21861f95f157887973819b6fa8522ee449e86e985481de352a81736999ad8e4b3b58fb72965683a7d2beb3442ba677d1a7a1c5d40'
