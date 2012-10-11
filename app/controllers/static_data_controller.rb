bb4e4b1cc982eeed0b79a217318b6ee90f718e2f
Jonathan Packer <jsp2151@columbia.edu>
2012-10-11 19:57:36 -0400

    Installed d3.js; created static_data_controller to serve static data files; created demo index.html.erb

---
+++ app/controllers/static_data_controller.rb
@@ -0,0 +1,41 @@
+class StaticDataController < ApplicationController
+    @@json_path = Rails.root.to_s + "/app/assets/data/"
+    
+    @@world_countries = File.read(@@json_path + "world-countries.geo.json")
+    
+    @@us_states = File.read(@@json_path + "us-states.geo.json")
+    @@contiguous_us_states = 
+        File.read(@@json_path + "contiguous-us-states.geo.json")
+        
+    @@us_state_centroids = 
+        File.read(@@json_path + "us-state-centroids.geo.json")
+        
+    @@us_counties = File.read(@@json_path + "us-counties.geo.json")
+    
+    @@test_arcs = File.read(@@json_path + "test-arcs.geo.json")
+    
+
+    def world_countries
+        render :json => @@world
+    end
+    
+    def us_states
+        render :json => @@us_states
+    end
+    
+    def contiguous_us_states
+        render :json => @@contiguous_us_states
+    end
+    
+    def us_state_centroids
+        render :json => @@us_state_centroids
+    end
+    
+    def us_counties
+        render :json => @@us_counties
+    end
+    
+    def test_arcs
+        render :json => @@test_arcs
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Geocraft::Application.routes.draw do
   get "home/index"
 
+  # Hack to serve static json, see static_json_controller.rb
+  match "/data/world-countries" => "static_data#world_countries"
+  match "/data/us-states" => "static_data#us_states"
+  match "/data/contiguous-us-states" => "static_data#contiguous_us_states"
+  match "/data/us-state-centroids" => "static_data#us_state_centroids"
+  match "/data/us-counties" => "static_data#us_counties"
+  match "/data/test-arcs" => "static_data#test_arcs"
+
   resources :etsy_sales_aggregates
 
   root :to => "home#index"
